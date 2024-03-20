167f2b066d629d732f5376c200fbad08fb42c261
Luis "Shadow" Sena <sena.luisgf@gmail.com>
2024-03-19 21:22:35 -0400

    fix: putting a try catch on log writer to catch no folder error

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -6,20 +6,24 @@ const LOG_DIR = process.env.LOG_DIR || 'logs'
 
 function Log(logFormat = 'simples') {
   return (req: Request, res: Response, next: NextFunction) => {
-    const now = new Date().toISOString();
-    const url = req.url;
-    const method = req.method;
-    let logMessage = `[${now}] ${method} ${url}`;
-
-    if (logFormat === 'completo') {
-      const httpVersion = `HTTP/${req.httpVersion}`;
-      const userAgent = req.headers['user-agent'];
-      logMessage += ` ${httpVersion} ${userAgent}`;
+    try {
+      const now = new Date().toISOString();
+      const url = req.url;
+      const method = req.method;
+      let logMessage = `[${now}] ${method} ${url}`;
+  
+      if (logFormat === 'completo') {
+        const httpVersion = `HTTP/${req.httpVersion}`;
+        const userAgent = req.headers['user-agent'];
+        logMessage += ` ${httpVersion} ${userAgent}`;
+      }
+  
+      // Salva o log em um arquivo, com um arquivo por dia
+      const logFileName = path.join(LOG_DIR, `access-${now.split('T')[0]}.log`);
+      fs.appendFileSync(logFileName, logMessage + '\n');
+    } catch (error) {
+      console.log("CRIE UMA PASTA VAZIA chamada 'logs' na raiz do projeto para salvar os logs da aplicação")
     }
-
-    // Salva o log em um arquivo, com um arquivo por dia
-    const logFileName = path.join(LOG_DIR, `access-${now.split('T')[0]}.log`);
-    fs.appendFileSync(logFileName, logMessage + '\n');
 
     next();
   }
